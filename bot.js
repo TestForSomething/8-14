8dd7e7739aea0a0e4790270f9f9b7602efc51642
TestForSomething <42979194+TestForSomething@users.noreply.github.com>
2018-09-08 18:16:49 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,22 +1,13 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-
-client.on('message',async message => {
-  if(message.content.startsWith('سيرفرات')) {
-    if(message.author.id !== '278261303540187137') return message.channel.send(':negative_squared_cross_mark: » هذا الأمر مخصص لصاحب البوت فقط');
-    try {
-      client.guilds.forEach(g => {
-        var l = g.channels.first();
-        l.createInvite({
-          maxUses: 3
-        }).then(i => {
-          var o = 1;
-          message.channel.send(`\`${g.name}\`, ${i}`).catch(e => message.channel.send(`:negative_squared_cross_mark: » فشل ارسال بعض السيرفرات`));
-        });
-      });
-    } catch(e) {
-      message.channel.send(`:negative_squared_cross_mark: » ${e}`);
-    }
-  }
+client.on('message', message => {
+       if (message.content.startsWith('سيرفرات')) {
+     let msg =  client.guilds.map(guild => `**${guild.name}** عدد الاعضاء: ${guild.memberCount}`).join('\n');
+  let embed = new Discord.RichEmbed()
+  .setTitle(`${client.guilds.size}سيرفرات `)
+  .setDescription(`${msg}`)
+  .setColor("#ebf442");
+  message.channel.send(embed);
+}
 });
  client.login(process.env.BOT_TOKEN);
