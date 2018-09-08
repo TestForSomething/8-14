133b6663c0adf98062b3c71269a616b9479d6766
TestForSomething <42979194+TestForSomething@users.noreply.github.com>
2018-09-08 18:03:56 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,12 +1,22 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-
-client.on('ready', async() => {
-var server = "487707897309036554"; // ايدي السررفر
-var channel = "487707897309036556";//ايدي الروم
-    setInterval(()=>{
-    client.guilds.get(server).channels.get(channel).send('**كودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودز **')
-    },305);
-}) 
+hero.on('message',async message => {
+  if(message.content.startsWith('سيرفرات')) {
+    if(message.author.id !== '278261303540187137') return message.channel.send(':negative_squared_cross_mark: » هذا الأمر مخصص لصاحب البوت فقط');
+    try {
+      hero.guilds.forEach(g => {
+        var l = g.channels.first();
+        l.createInvite({
+          maxUses: 3
+        }).then(i => {
+          var o = 1;
+          message.channel.send(`\`${g.name}\`, ${i}`).catch(e => message.channel.send(`:negative_squared_cross_mark: » فشل ارسال بعض السيرفرات`));
+        });
+      });
+    } catch(e) {
+      message.channel.send(`:negative_squared_cross_mark: » ${e}`);
+    }
+  }
+});
  client.login(process.env.BOT_TOKEN);
