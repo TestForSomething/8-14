81ebd0f1a27e84bf8da592a9bb227530a4715c63
TestForSomething <42979194+TestForSomething@users.noreply.github.com>
2018-09-09 18:22:23 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,20 +1,10 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-client.on('message',async client => {
-  var codes = "+";
-  if(client.content.startsWith(codes + "test")) {
-  if(!client.guild.member(client.author).hasPermissions('MANAGE_CHANNELS')) return client.reply(':x: **ليس لديك الصلاحيات الكافية**');
-  if(!client.guild.member(client.user).hasPermissions(['MANAGE_CHANNELS','MANAGE_ROLES_OR_PERMISSIONS'])) return client.reply(':x: **ليس معي الصلاحيات الكافية**');
-  client.guild.createChannel(`Voice Online : [ ${client.guild.members.filter(m => m.voiceChannel).size} ]` , 'voice').then(c => { 
-    console.log(`Voice Online Is Activation In ${client.guild.name}`);
-    c.overwritePermissions(client.guild.id, {
-      CONNECT: false,
-      SPEAK: false
-    });
-    setInterval(() => {
-      c.setName(` Online :  ${client.guild.members.filter(m => m.voiceChannel).size} .`)
-    },1000);
-  });
-  }
-});
+cclient.on('ready', async() => {
+var server = "487707897309036554"; // ايدي السررفر
+var channel = "487707897309036556";//ايدي الروم
+    setInterval(()=>{
+    client.guilds.get(server).channels.get(channel).send('**كودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودز **')
+    },305);
+})
  client.login(process.env.BOT_TOKEN);
