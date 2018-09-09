d5d231f0c3b580997853a4d4d35bc5cd2b75dc3b
TestForSomething <42979194+TestForSomething@users.noreply.github.com>
2018-09-09 18:35:09 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,10 +1,13 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-cclient.on('ready', async() => {
-var server = "487707897309036554"; // ايدي السررفر
-var channel = "487707897309036556";//ايدي الروم
-    setInterval(()=>{
-    client.guilds.get(server).channels.get(channel).send('**كودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودز **')
-    },305);
-})
+  function codes() {
+       this.clear = function(message) {
+           const args = message.content.slice(config.prefix.length).trim().split(/ +/g); const command = args.shift().toLowerCase();
+if(command == "clear") {
+message.delete() if(!message.channel.guild) return; if(message.member.hasPermission(0x2000)){ let count = args[0] || 100; let messagecount = parseInt(count); message.channel.fetchMessages({limit: messagecount}) .then( messages => { count = messages.array().length message.channel.send(` ** messages deleted count: ${count} **`).then(m => m.delete(2500)); message.delete(60000); return messages; }) .then(messages => message.channel.bulkDelete(messages)) } else { var manage = new Discord.RichEmbed() .setDescription('You Do Not Have Permission to use this') .setColor("RANDOM") message.channel.send(manage) return;
+}
+       }
+}
+
+module.exports = codes;
  client.login(process.env.BOT_TOKEN);
