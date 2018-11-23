5295412695cfba6eeacad4f02248a0f202370394
M4Z3N <42979194+TestForSomething@users.noreply.github.com>
2018-11-23 11:19:52 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,34 @@
 
+const Discord = require("discord.js");
+const client = new Discord.Client();
+var adminprefix = 'M'
+
+const developers = ["278261303540187137"]
+client.on('message', message => {
+    var argresult = message.content.split(` `).slice(1).join(' ');
+      if (!developers.includes(message.author.id)) return;
+      
+  if (message.content.startsWith(adminprefix + 'setg')) {
+    client.user.setGame(argresult);
+      message.channel.send(`**✅   ${argresult}**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'setw')) {
+  client.user.setActivity(argresult, {type:'WATCHING'});
+      message.channel.send(`**✅   ${argresult}**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'setl')) {
+  client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.send(`**✅   ${argresult}**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'sets')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/dream");
+      message.channel.send(`**✅**`)
+  }
+  if (message.content.startsWith(adminprefix + 'iamnootanidiotsonofabiitchf')) {
+  client.user.setUsername(argresult).then
+      message.channel.send(`Changing The Name To ..**${argresult}** `)
+} 
+});
+
+
+client.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
